be1726bdc3d5775bb7464680d203c5fd44faf8ca
ridwan <ridwanediallo99@gmail.com>
2022-09-07 18:13:01 +0000

    Update Label class

--- classes/label.rb
+++ classes/label.rb
@@ -1,5 +1,5 @@
 class Label
-  attr_reader :id, items
+  attr_reader :id, :items
   attr_accessor :title, :color
   def initialize(title, color)
     @id =  Random.rand(1..1000)
@@ -10,5 +10,6 @@ class Label
 
 
   def add_item(item)
+    @items << item unless @items.include?(item)
   end
 end
