3fe08f730591c6771dc68604a00f3e8e9f2992c8
unknown <abodyalex1@gmail.com>
2022-09-07 16:50:19 +0300

    Add classes

---
+++ spec/music_spec.rb
@@ -0,0 +1,15 @@
+require './classes/musicalbum'
+describe Book do
+  before :each do
+    @musicalbum = MusicAlbum.new 'Freedom',  '2019', true
+  end
+  it 'should print the alboum name' do
+    expect(@musicalbum.name).to eq 'Freedom'
+  end
+  it 'should print the publish date' do
+    expect(@musicalbum.publish_date).to eq '2019'
+  end
+  it 'should print if it on spotify or not' do
+    expect(@musicalbum.on_spotify).to eq true
+  end
+end
