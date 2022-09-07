e3caac14a9016ae4a94d93751912443bee524a4e
ridwan <ridwanediallo99@gmail.com>
2022-09-07 21:39:29 +0000

    Add BookPersist module

---
+++ data_persist/book_persist.rb
@@ -0,0 +1,23 @@
+require "json"
+require_relative "../classes/book"
+require_relative "handle_json"
+
+module BookPersist
+  def persist_book
+    books_arr = []
+    @books.each do |book|
+      books_arr << {
+        publisher: book.publisher,
+        publish_date: book.publish_date,
+        cover_state: book.cover_state
+      }
+    end
+    write_json(books_arr, "books.json")
+  end
+  def load_books
+    file = File.read("books.json")
+    JSON
+      .parse(file)
+      .each { |book| @books.push(Book.new(book["publisher"], book["publish_date"], book["cover_state"])) }
+  end
+end
