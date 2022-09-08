9b594796c5a77665959357b6d2f1803efc133e4e
ridwan <ridwanediallo99@gmail.com>
2022-09-08 11:55:09 +0000

    Add load_labels

--- data_persist/label_persist.rb
+++ data_persist/label_persist.rb
@@ -6,11 +6,15 @@ module LabelPersist
   def persist_label
     labels_arr = []
     @labels.each do |label|
-      labels_arr << {
-        title: label.title,
-        color: label.color,
-      }
+      labels_arr << { title: label.title, color: label.color }
     end
     write_json(labels_arr, "labels.json")
+  end
+
+  def load_labels
+    file = File.read("labels.json")
+    JSON
+      .parse(file)
+      .each { |label| @labels.push(Label.new(label["title"], label["color"])) }
   end
 end
