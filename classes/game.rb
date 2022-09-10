153f7a354445ad125bd9274586b8f0578e8afed3
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-09-10 18:04:16 +0530

    Updated the code in author item label game

--- classes/game.rb
+++ classes/game.rb
@@ -1,13 +1,17 @@
-class Game
-    attr_accessor :multiplayer, :last_played_at
-    def initialize(multiplayer, last_played_at)
-       @multiplayer = multiplayer
-       @last_played_at = last_played_at
-    end
+require_relative "item"
+require "date"
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+  def initialize(multiplayer, last_played_at)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
   private
 
   def can_be_archived?
-   super && @last_played_at > 2
+    super && Time.now.year - Date.parse(@publish_date).year > 2
   end
 end
