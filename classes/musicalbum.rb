d8dd32d436102c01fb1d038af5114726644f7dcb
unknown <abodyalex1@gmail.com>
2022-09-10 15:24:24 +0300

    Add tests

--- classes/musicalbum.rb
+++ classes/musicalbum.rb
@@ -11,8 +11,9 @@ class MusicAlbum < Item
     @on_spotify = on_spotify
   end
 
+  private
+
   def can_be_archived?
-    
     super && @on_spotify == true
   end
 end
--- spec/genre.spec.rb
+++ spec/genre.spec.rb
@@ -6,4 +6,13 @@ describe Genre do
   it 'should print the genre name' do
     expect(@genre.names).to eq 'comedy'
   end
+
+  it 'should print the genre id' do
+    expect(@genre.id).to be_between(1, 1000)
+  end
+
+  it 'should print the genre items' do
+    expect(@genre.items).to eq []
+  end
+
 end
